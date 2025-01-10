032f5930038102ec465d18e335008641e6b1a1b9
Bee Bee Wijaya <beebeewijaya.tech@gmail.com>
2025-01-10 22:01:30 +0700

    task: adding sleep to prevent rate limiter

--- controllers/copy.py
+++ controllers/copy.py
@@ -1,7 +1,12 @@
+import time
+import random
+
 def loop_through_files_upload(drive, parent_folder_id, new_parent_folder_id):
   listFiles = drive.list_files_from_parent_folder(parent_folder_id)
 
   for f in listFiles:
+    duration = random.uniform(1.5, 3.0)
+
     isFolder = f["mimeType"] == "application/vnd.google-apps.folder"
     if isFolder:
       id = drive.create_folder(f["name"], new_parent_folder_id)
@@ -10,3 +15,5 @@ def loop_through_files_upload(drive, parent_folder_id, new_parent_folder_id):
       f_id = f["id"]
       f_name = f["name"]
       drive.copy_file(f_id, f_name, new_parent_folder_id)
+
+    time.sleep(duration)
--- controllers/download.py
+++ controllers/download.py
@@ -1,9 +1,12 @@
 import os
+import time
+import random
 
 def loop_through_files_download(drive, parent_folder_id, folder):
   listFiles = drive.list_files_from_parent_folder(parent_folder_id)
 
   for f in listFiles:
+    duration = random.uniform(1.5, 3.0)
     isFolder = f["mimeType"] == "application/vnd.google-apps.folder"
     if isFolder:
       fName = os.path.join(folder, f["name"])
@@ -14,3 +17,5 @@ def loop_through_files_download(drive, parent_folder_id, folder):
     else:
       print(f)
       drive.downloadfiles(f["id"], f["name"], folder)
+
+    time.sleep(duration)
